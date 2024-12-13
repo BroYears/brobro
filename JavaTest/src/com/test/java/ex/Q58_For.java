d65b2d2e3ad534def3b689fcc9b0945de1595ba6
user <user@DESKTOP-9BP7BN4>
2024-12-13 17:51:42 +0900

    asd

---
+++ JavaTest/src/com/test/java/ex/Q58_For.java
@@ -0,0 +1,50 @@
+package com.test.java.ex;
+
+public class Q58_For {
+	
+		public static void main(String[] args) {
+			
+			int sum = 0;
+			
+			for (;;) {
+			
+				for (int i=1; i<=100; i++) {
+					
+					sum += i;
+					switch (i) {
+						case 10:
+						System.out.println(sum);
+						break;
+						case 20:
+						System.out.println(sum);
+						break;
+						case 30:
+						System.out.println(sum);
+						break;
+						case 40:
+						System.out.println(sum);
+						break;
+						case 50:
+						System.out.println(sum);
+						break;
+						case 60:
+						System.out.println(sum);
+						break;
+						case 70:
+						System.out.println(sum);
+						break;
+						case 80:
+						System.out.println(sum);
+						break;
+						case 90:
+						System.out.println(sum);
+						break;
+						case 100:
+						System.out.println(sum);
+						break;
+					} 
+				}
+			}
+		}//main
+
+}
